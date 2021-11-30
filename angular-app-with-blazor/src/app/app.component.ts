04b9ee45004cdd8d3882e4a94c753e1324bda71c
Connor Schmidt <schmidty68connor@gmail.com>
2021-11-29 22:23:00 -0600

    Added demo angular project. Moved blazor-adapter component. Added after-build target to copy Angular components.

---
+++ angular-app-with-blazor/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-app-with-blazor';
+
+  blazorCounters: any[] = [];
+  nextCounterIndex = 1;
+
+  addBlazorCounter() {
+    const index = this.nextCounterIndex++;
+    this.blazorCounters.push({
+      title: `Counter ${index}`,
+      incrementAmount: index,
+      customObject: { StringValue: 'Hello!', IntegerValue: 42 }
+    });
+  }
+
+  removeBlazorCounter() {
+    this.blazorCounters.pop();
+  }
+
+  modifyParameters() {
+    for (const counter of this.blazorCounters) {
+      counter.incrementAmount++;
+
+      let { StringValue, IntegerValue } = counter.customObject;
+
+      StringValue += '!';
+      IntegerValue -= 1;
+
+      counter.customObject = { StringValue, IntegerValue };
+    }
+  }
+
+  customCallback(mouseEventArgs: any) {
+    console.log(`Custom callback: ${JSON.stringify(mouseEventArgs)}`);
+  }
+}
