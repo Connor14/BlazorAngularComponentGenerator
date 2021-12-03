d286971dfa6459d1d2165427557bbb5e42c5955c
Connor Schmidt <schmidty68connor@gmail.com>
2021-12-02 21:22:34 -0600

    Improved the build process. Added targets for osx-x64 and linux-x64

---
+++ delete-bin-obj.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Deleting bin and obj folders
+
+rem Source: https://stackoverflow.com/a/755433
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+echo Done
